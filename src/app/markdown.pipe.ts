e8e01db04ac145783010d3aa1f7487779b222b4d
George Ionas <georgeion@Georges-MacBook-Pro.local>
2020-09-14 16:35:04 -0400

    all content rendering functional

--- src/app/markdown.pipe.ts
+++ src/app/markdown.pipe.ts
@@ -1,10 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as showdown from "showdown";
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import DOMPurify from 'dompurify';
+
 
 @Pipe({name: 'markdownTranslater'})
 export class MarkdownPipe implements PipeTransform {
-  transform(value: string): string {
-    var converter = new showdown.Converter();
-    return converter.makeHtml(value);
-  }
+  
+    constructor(protected sanitizer: DomSanitizer) {}
+
+    public transform(value: string): SafeHtml {
+        var converter = new showdown.Converter();
+        const htmlFromMarkdown = converter.makeHtml(value);
+        const sanitizedHtml = DOMPurify.sanitize(htmlFromMarkdown);
+        return this.sanitizer.bypassSecurityTrustHtml(sanitizedHtml);
+    }
 }
